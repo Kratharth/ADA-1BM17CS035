087806b9da7f7342fd87f17962c08be9ae466e26
Kratharth <kratharthhegde.cs17@bmsce.ac.in>
2019-09-12 13:56:36 +0530

    island problem

--- graph/island.py
+++ graph/island.py
@@ -1,44 +1,66 @@
-﻿#global variables
-rowNbr = [-1,-1,-1,0,0,1,1,1]
-colNbr = [-1,0,1,-1,1,-1,0,1]
-
-
-def isSafe(adj_mat, row, col, visited_mat):
-	if row>=0 and row<n and col>=0 and col<n:
-		if adj_mat[row][col]==1  and visited_mat[row][col]== 0:
+def issafe(i,j):
+	global rows
+	global cols
+	global marked
+	global graph
+	if i >= 0 and i < rows and j>=0 and j<cols:
+		if graph[i][j] == 1 and marked[i][j] == 0:
+			print('Entering'+' '+str(graph[i][j])+' '+'at '+str(i)+' '+str(j))
+			marked[i][j]=1
 			return True
+		else:
+			marked[i][j]=1
+			return False
 	else:
 		return False
 
-def dfs(adj_mat, i, j, visited_mat):
-	global rowNbr
-	global colNbr
-	visited_mat[i][j] = True
-	for k in range(8):
-		if isSafe(adj_mat, i+rowNbr[k], j+colNbr[k], visited_mat):
-			if( i!= j):
-				dfs(adj_mat, i+rowNbr[k], j+colNbr[k], visited_mat)
+
+def des(i,j):
+	global marked
+	global count
+	marked[i][j] = 1
+	if issafe(i-1,j-1):
+		des(i-1,j-1)
+	if issafe(i-1,j):
+		des(i-1,j)
+	if issafe(i-1,j+1):
+		des(i-1,j+1)
+	if issafe(i,j-1):
+		des(i,j-1)
+	if issafe(i,j+1):
+		des(i,j+1)
+	if issafe(i+1,j-1):	
+		des(i+1,j-1)
+	if issafe(i+1,j):	
+		des(i+1,j)
+	if issafe(i+1,j+1):
+		des(i+1,j+1)
 
 
-if __name__ == "__main__":
-	adj_mat	= []
-	visited_mat = []
-	island_count = 0
-	rows = 0
-	cols = 0
-	rows = int(input('enter the number of rows'))
-	cols = int(input('enter the number of columns'))
-	for _ in range(rows):
-		l = list(map(int, input().split()))
-		adj_mat.append(l)
-	
-	for i in range(rows):
-		for j in range(len(adj_mat[i])):
-			visited_mat[i][j] = 0	
 
-	for i in range(rows):
-		for j in range(len(adj_mat[i])):
-			if adj_mat[i][j] == 1 and visited_mat[i][j] == False:
-				dfs(adj_mat, i, j, visited_mat)
-				island_count += 1
-	print(island_count)			
+graph = []
+marked = []
+rows = int(input('enter the number of rows'))
+cols = int(input('enter the number of columns'))
+count = 0
+# matrix input
+print('enter the matrix (one row in one line)')
+for i in range(rows):
+	lis=list(map(int,input('enter the '+str((i+1))+'row').split()))
+	graph.append(lis)
+
+# marking the entire matrix as zero
+
+for i in range(rows):
+	lis = [0]*cols
+	marked.append(lis)
+print(marked)
+print(graph)
+# checking for number of islands
+for i in range(rows):
+	for j in range(cols):
+		if marked[i][j] == 0:
+			if graph[i][j] == 1 and i != j:
+				count = count + 1
+			des(i,j)
+print(count) 		
